24c4e1edea4a7193506400d537736dfe2795e579
Artur Hallmann <arturh@arturh.de>
2021-11-20 21:50:59 +0100

    fixed healthbar display

--- World.gd
+++ World.gd
@@ -20,6 +20,7 @@ signal co2_level_changed
 
 var fire_effects = {}
 var tree_entities = {}
+var building_entities = {}
 
 var rng = RandomNumberGenerator.new()
 
@@ -124,6 +125,10 @@ func add_fire(pos: Vector3):
 				add_child(tree_instance)
 				tree_entities[pos] = tree_instance
 			tree_entities[pos].on_catch_fire()
+		var building_content = BuildingMap.get_cell_item(pos.x, pos.y, pos.z)
+		if building_content != -1:
+			if building_entities.has(pos):
+				building_entities[pos].on_catch_fire()
 				
 func on_tree_burndown(pos):
 	if fire_effects.has(pos):
