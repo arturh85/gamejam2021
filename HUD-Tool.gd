d6b256ee2e5cc264ac5730e56100bac5559cd8c1
Artur Hallmann <arturh@arturh.de>
2021-11-21 03:57:33 +0100

    central Buildings and cost definitions

--- HUD-Tool.gd
+++ HUD-Tool.gd
@@ -14,3 +14,30 @@ func _ready():
 		options.add_item("Silo", 8)
 
 		options.select(0)
+		_on_OptionButton_item_selected(0)
+
+
+func _on_OptionButton_item_selected(index):
+	var world = $"../.."
+	var cost = null
+	if index == 0:
+		cost = world.fire_costs
+	elif index == 1:
+		cost = world.tree_costs
+	elif index == 2:
+		cost = world.bulldozer_costs
+	elif index == 3:
+		cost = world.building_costs[world.Buildings.SOLAR_CELL]
+	elif index == 4:
+		cost = world.building_costs[world.Buildings.BATTERY]
+	elif index == 5:
+		cost = world.powerline_costs
+	elif index == 6:
+		cost = world.building_costs[world.Buildings.FARM]
+	elif index == 7:
+		cost = world.building_costs[world.Buildings.WATER_TOWER]
+	elif index == 8:
+		cost = world.building_costs[world.Buildings.SILO]
+		
+	$"Label-Energy".text = str(cost["energy"])
+	$"Label-Resources".text = str(cost["resources"])
