ebcec826b4e8dcf20dae839cf625f6817ddbc7ef
Nils Keller <nils@inlage.com>
2021-11-22 00:30:08 +0100

    icons/hud

--- HUD-Tool.gd
+++ HUD-Tool.gd
@@ -10,23 +10,7 @@ func _on_HUDStatus_mouse_exited():
 	$"../../".mouseInHUD = false
 	
 func _ready():
-	var world = gamestate.world()
-	var options = $OptionButton
-	if options.items.size() == 0:
-		options.add_item("Fire", 0)
-		options.add_item("Tree", 1)
-		options.add_item("Bulldozer", 2)
-		options.add_item("SolarCell", 3)
-		options.add_item("Battery", 4)
-		options.add_item("PowerLine", 5)
-		options.add_item("Farm", 6)
-		options.add_item("WaterTower", 7)
-		options.add_item("Silo", 8)
-		options.add_item("Meteor", 9)
-		options.add_item("Cloud", 10)
-
-		options.select(0)
-		_on_OptionButton_item_selected(0)
+	pass
 
 
 func _on_OptionButton_item_selected(index):
